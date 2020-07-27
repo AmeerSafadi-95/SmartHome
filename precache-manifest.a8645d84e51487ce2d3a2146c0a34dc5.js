247e6773b7e3429c2257e48aee692a89c77cc8aa
AmeerSafadi-95 <ameersafadi95@gmail.com>
2020-07-27 23:32:22 +0300

    Updates

---
+++ precache-manifest.a8645d84e51487ce2d3a2146c0a34dc5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e096175dc7e37853c1d92cd95955179b",
+    "url": "/SmartHome/index.html"
+  },
+  {
+    "revision": "33782cafe84e20c836c4",
+    "url": "/SmartHome/static/css/main.e1cdde8f.chunk.css"
+  },
+  {
+    "revision": "59c075a7a9b526a7896e",
+    "url": "/SmartHome/static/js/2.e867aef2.chunk.js"
+  },
+  {
+    "revision": "a51e1a732616a65fe21698cc04856fb1",
+    "url": "/SmartHome/static/js/2.e867aef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33782cafe84e20c836c4",
+    "url": "/SmartHome/static/js/main.09b9b241.chunk.js"
+  },
+  {
+    "revision": "6719f7b7ebc4e5166749",
+    "url": "/SmartHome/static/js/runtime-main.be3964d8.js"
+  },
+  {
+    "revision": "234cce4bfb74ab41fc5ed51bd1fe1d05",
+    "url": "/SmartHome/static/media/s7.234cce4b.jpg"
+  },
+  {
+    "revision": "57ded8e3a070c88e8fa83fe6a80f86de",
+    "url": "/SmartHome/static/media/s8.57ded8e3.jpg"
+  },
+  {
+    "revision": "b8056e17f12b05f03fa14c70ebf88fab",
+    "url": "/SmartHome/static/media/s9.b8056e17.jpg"
+  }
+]);
